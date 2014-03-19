5df4f4b33e9996678f23500736a668457a8ca6ef
domu1de <domenic@muskulus.eu>
2014-03-19 11:57:04 +0100

    Adding initial structure, travis-ci, phpunit and composer

---
+++ src/YAM.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of YAM.
+ *
+ * @author     Domenic Muskulus <domenic@muskulus.eu>
+ * @copyright  2014 Domenic Muskulus
+ * @license    MIT
+ */
+
+namespace YAM;
+
+class YAM
+{
+}
