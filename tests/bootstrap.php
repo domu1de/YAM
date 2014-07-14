b0e74afd2628a7a9493e5ae43c2e8ba1e8a1ae4b
domu1de <domenic@muskulus.eu>
2014-07-14 13:33:45 +0200

    Adding coveralls.io support

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
+$loader->addPsr4('YAM\\UnitTest\\', __DIR__ . '/Unit/');
+$loader->addPsr4('YAM\\IntegrationTest\\', __DIR__ . '/Integration/');
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
